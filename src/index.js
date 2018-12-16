01653661ff0591f155bafdadd7a3c38d345422d0
Mengnan Shi <thundersmn@outlook.com>
2018-12-16 21:30:17 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './js/presentational/App';
+import store from './js/store/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
